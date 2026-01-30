0abf787a2a5e1def5d1c1066761d778e6b5febec
kmutamar <110453757+kmutamar@users.noreply.github.com>
2026-01-30 16:41:19 +0700

    Add files via upload

---
+++ trapesium_v1.m
@@ -0,0 +1,34 @@
+clear all;
+close all;
+clc;
+
+% interval integral
+a=2;
+b=7;
+n=4;
+h=(b-a)/n;
+
+% realokasi
+x=zeros(n+1,1);
+f=zeros(n+1,1);
+
+% jumlahan awal
+x(1)=a;
+x(n+1)=b;
+f(1)=x(1)^2-6*x(1)+8;
+f(n+1)=x(n+1)^2-6*x(n+1)+8;
+jumlah=f(1)+f(n+1);
+
+%jumlahan final
+for i=2:n
+    x(i)=a+(i-1)*h;
+    f(i)=x(i)^2-6*x(i)+8;
+    jumlah=jumlah+2*f(i);
+end
+hasil=jumlah*h/2;
+disp(['Hasil integral = ',num2str(hasil,10)])
+
+
+
+
+
